29f2364c87b3c72d6d094a5714bbbcadebdb621f
Andreas Kyriakopoulos <a.kyriakopoulos@xe.gr>
2023-02-18 16:49:30 +0200

    add FieldsViewModel.Factory (injecting from AppContainer.fieldsRepository)

--- app/src/main/java/gr/unimatch/android/viewmodel/FieldsViewModel.kt
+++ app/src/main/java/gr/unimatch/android/viewmodel/FieldsViewModel.kt
@@ -2,6 +2,10 @@ package gr.unimatch.android.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import gr.unimatch.android.UniMatchApplication
 import gr.unimatch.android.repository.FieldsRepository
 
 class FieldsViewModel(
@@ -10,4 +14,22 @@ class FieldsViewModel(
 ) : ViewModel() {
     val fields = fieldsRepository.getFields()
 
+    companion object {
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras,
+            ): T {
+                val application =
+                    checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
+                val savedStateHandle = extras.createSavedStateHandle()
+
+                return FieldsViewModel(
+                    savedState = savedStateHandle,
+                    fieldsRepository = (application as UniMatchApplication).appContainer.fieldsRepository,
+                ) as T
+            }
+        }
+    }
 }
