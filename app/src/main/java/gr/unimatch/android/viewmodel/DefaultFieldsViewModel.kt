d20ca498c9d378aa79e458eb904ae276975928ec
Andreas Kyriakopoulos <a.kyriakopoulos@xe.gr>
2023-02-20 23:01:27 +0200

    make FieldsViewModel interface with DefaultFieldsViewModel as implementation

---
+++ app/src/main/java/gr/unimatch/android/viewmodel/DefaultFieldsViewModel.kt
@@ -0,0 +1,24 @@
+package gr.unimatch.android.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import gr.unimatch.android.common.createViewModelFactory
+import gr.unimatch.android.repository.FieldsRepository
+
+class DefaultFieldsViewModel(
+    private val savedState: SavedStateHandle,
+    private val fieldsRepository: FieldsRepository,
+) : ViewModel(), FieldsViewModel {
+    override val fields = fieldsRepository.getFields()
+
+    companion object {
+        val Factory: ViewModelProvider.Factory =
+            createViewModelFactory { savedStateHandle, appContainer ->
+                DefaultFieldsViewModel(
+                    savedState = savedStateHandle,
+                    fieldsRepository = appContainer.fieldsRepository,
+                )
+            }
+    }
+}
--- app/src/main/java/gr/unimatch/android/viewmodel/FieldsViewModel.kt
+++ app/src/main/java/gr/unimatch/android/viewmodel/FieldsViewModel.kt
@@ -1,24 +1,8 @@
 package gr.unimatch.android.viewmodel
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import gr.unimatch.android.common.createViewModelFactory
-import gr.unimatch.android.repository.FieldsRepository
+import androidx.lifecycle.LiveData
+import gr.unimatch.android.database.entity.Field
 
-class FieldsViewModel(
-    private val savedState: SavedStateHandle,
-    private val fieldsRepository: FieldsRepository,
-) : ViewModel() {
-    val fields = fieldsRepository.getFields()
-
-    companion object {
-        val Factory: ViewModelProvider.Factory =
-            createViewModelFactory { savedStateHandle, appContainer ->
-                FieldsViewModel(
-                    savedState = savedStateHandle,
-                    fieldsRepository = appContainer.fieldsRepository,
-                )
-            }
-    }
+interface FieldsViewModel {
+    val fields: LiveData<List<Field>>
 }
