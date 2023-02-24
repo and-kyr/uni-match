1a2170071fde60b5e684d6519ee9f7b7dabbeaf1
Andreas Kyriakopoulos <a.kyriakopoulos@xe.gr>
2023-02-24 19:07:34 +0200

    FieldsViewModel: add selectedFields to and its implementation

--- app/src/main/java/gr/unimatch/android/viewmodel/DefaultFieldsViewModel.kt
+++ app/src/main/java/gr/unimatch/android/viewmodel/DefaultFieldsViewModel.kt
@@ -1,11 +1,12 @@
 package gr.unimatch.android.viewmodel
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 import gr.unimatch.android.common.createViewModelFactory
+import gr.unimatch.android.database.entity.Field
 import gr.unimatch.android.repository.CollegeFieldsRepository
 import gr.unimatch.android.repository.FieldsRepository
+import gr.unimatch.android.viewmodel.FieldsViewModel.Companion.DEFAULT_FIELDS
+import gr.unimatch.android.viewmodel.FieldsViewModel.Companion.SELECTED_FIELDS_KEY
 
 class DefaultFieldsViewModel(
     private val savedState: SavedStateHandle,
@@ -13,6 +14,27 @@ class DefaultFieldsViewModel(
     private val collegeFieldsRepository: CollegeFieldsRepository,
 ) : ViewModel(), FieldsViewModel {
     override val fields = fieldsRepository.getFields()
+
+    private var selectedFieldsValue
+        get() = savedState[SELECTED_FIELDS_KEY]
+            ?: DEFAULT_FIELDS
+        set(value) {
+            savedState[SELECTED_FIELDS_KEY] = value
+        }
+
+    override val selectedFields: LiveData<Set<Field>>
+        get() = savedState.getLiveData(
+            key = SELECTED_FIELDS_KEY,
+            initialValue = DEFAULT_FIELDS,
+        ).distinctUntilChanged()
+
+    override fun onFieldAdded(field: Field) {
+        selectedFieldsValue = selectedFieldsValue + field
+    }
+
+    override fun onFieldRemoved(field: Field) {
+        selectedFieldsValue = selectedFieldsValue - field
+    }
 
     companion object {
         val Factory: ViewModelProvider.Factory =
--- app/src/main/java/gr/unimatch/android/viewmodel/FieldsViewModel.kt
+++ app/src/main/java/gr/unimatch/android/viewmodel/FieldsViewModel.kt
@@ -5,4 +5,13 @@ import gr.unimatch.android.database.entity.Field
 
 interface FieldsViewModel {
     val fields: LiveData<List<Field>>
+    val selectedFields: LiveData<Set<Field>>
+
+    fun onFieldAdded(field: Field)
+    fun onFieldRemoved(field: Field)
+
+    companion object {
+        const val SELECTED_FIELDS_KEY = "selectedFields"
+        val DEFAULT_FIELDS = setOf<Field>()
+    }
 }
